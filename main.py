8a908a8f516c259ee59fb3dbbfea742f724f52ba
AutoMArket <automarket0123456789@gmail.com>
2021-11-05 13:14:09 +0300

    sampla_1

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wgwoiqersgwsdj:633384896066aebffe4dfe75a967d128f8927fab7f5c3a1016badec86fe9dfa0@ec2-34-202-66-20.compute-1.amazonaws.com:5432/d1spnuqe04ggu5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
